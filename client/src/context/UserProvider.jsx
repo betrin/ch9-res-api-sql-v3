6f1a57f01bd6824046138a082b176c9187682aed
Android B <10996124>
2025-06-25 22:18:42 +0100

    Separate UserContext from Provider

---
+++ client/src/context/UserProvider.jsx
@@ -0,0 +1,50 @@
+import { useState } from "react";
+import Cookies from "js-cookie";
+import { api } from "../utils/apiHelper";
+import { UserContext } from "./UserContext";
+
+export const UserProvider = ({ children }) => {
+  const cookie = Cookies.get("authedUser");
+  const storedCredentials = sessionStorage.getItem("sessionCredentials");
+  
+  const [authUser, setAuthUser] = useState(cookie ? JSON.parse(cookie) : null);
+  // Store credentials in sessionStorage for persistence across navigation
+  const [sessionCredentials, setSessionCredentials] = useState( storedCredentials ? JSON.parse(storedCredentials) : null);
+  
+  const signIn = async (userCredentials) => {
+    // console.log('UserContext - signIn called with:', userCredentials);
+    const response = await api('/users', 'GET', null, userCredentials)
+    if (response.status === 200) {
+      const user = await response.json();
+      // console.log('User: ', user);
+      setAuthUser(user);
+      // Store credentials in sessionStorage
+      setSessionCredentials(userCredentials);
+      sessionStorage.setItem("sessionCredentials", JSON.stringify(userCredentials));
+      Cookies.set("authedUser", JSON.stringify(user), { expires: 1 });
+      return user;
+    } else if (response.status === 401) {
+      return null;
+    } else {
+      throw new Error("Error with the request");
+    }
+  };
+
+  const signOut = () => {
+    setAuthUser(null);
+    setSessionCredentials(null);
+    Cookies.remove('authedUser'); 
+    sessionStorage.removeItem("sessionCredentials");
+  }
+
+  return (
+    <UserContext.Provider value={{
+      authUser,
+      sessionCredentials,
+      signIn,
+      signOut,
+      }}>
+      {children}
+    </UserContext.Provider>
+  )
+}; 
--- client/src/main.jsx
+++ client/src/main.jsx
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
 
 // Context
-import { UserProvider } from './context/UserContext'
+import { UserProvider } from './context/UserProvider'
 
 // Styles
 import './styles/reset.css'
